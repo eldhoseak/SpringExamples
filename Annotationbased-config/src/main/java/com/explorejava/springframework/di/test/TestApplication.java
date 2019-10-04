134ffd3009073e687e6e314efdee2d0d24a43f81
elabraham <v-elabraham@expedia.com>
2019-10-04 18:11:11 +0530

    Annotation based configuration

--- Annotationbased-config/src/main/java/com/explorejava/springframework/di/test/TestApplication.java
+++ Annotationbased-config/src/main/java/com/explorejava/springframework/di/test/TestApplication.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.explorejava.springframework.di.config.AppConfiguration;
 import com.explorejava.springframework.di.service.UserService;
 
+import java.util.Arrays;
+
 public class TestApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
+		Arrays.asList(applicationContext.getBeanDefinitionNames()).stream().forEach(System.out::println);
 		UserService  userService = applicationContext.getBean(UserService.class);
 		userService.processMsg("twitter message sending ");
 	}
