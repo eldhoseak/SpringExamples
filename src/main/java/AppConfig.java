d8097d5c3cdefa5e21092821a6d1883997596537
elabraham <v-elabraham@expedia.com>
2019-10-04 12:57:38 +0530

    Initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	@Bean(initMethod = "init", destroyMethod = "destroy")
+	public DatabaseInitiaizer databaseInitiaizer() {
+		return new DatabaseInitiaizer();
+	}
+}
---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
+		context.close();
+	}
+}
